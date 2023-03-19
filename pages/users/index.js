3727b39becbb7edc8028b66b0d192be004db46cd
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-03-19 18:09:15 +0100

    Added json-server and fixed fetch bug

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,20 +1,27 @@
-function Users({ posts }) {
+function Users({ users }) {
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>{post.name}</div>
-      ))}
+    <div className="w-9/12 mx-auto mt-20">
+      <h1 className="text-3xl font-bold">List Of Users</h1>
+      <div className="grid grid-cols-4 gap-3">
+        {users.map((user) => (
+          <div key={user.id} className="my-4 p-4 shadow-md rounded-md">
+            <p className="text-xl font-semibold py-3">{user.name}</p>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await res.json();
+  const res = await fetch("http://localhost:8000/users");
+  const data = await res.json();
 
   return {
     props: {
-      posts,
+      users: data,
     },
   };
 }
