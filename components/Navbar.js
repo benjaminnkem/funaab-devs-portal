4574cf4f14736ebe9782366c335ff31a42b2f969
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-29 02:08:41 +0100

    Reconstructed Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,54 +1,12 @@
 import Image from "next/image";
 import navStyles from "./styles/navbar.module.css";
-import { useRef } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
 const Navbar = ({ bgColor, textColor }) => {
-  const navIconRef = useRef(null);
-  const menuElRef1 = useRef(null);
-  const menuElRef2 = useRef(null);
-  const menuElRef3 = useRef(null);
-  const menuElRef4 = useRef(null);
-  const menuElRef5 = useRef(null);
-
-  function expandMenu() {
-    navIconRef.current.style.opacity = "1";
-    navIconRef.current.style.zIndex = "20";
-
-    menuElRef1.current.style.opacity = "1";
-    menuElRef1.current.style.transform = "translateY(0)";
-
-    menuElRef2.current.style.opacity = "1";
-    menuElRef2.current.style.transform = "translateY(0)";
-
-    menuElRef3.current.style.opacity = "1";
-    menuElRef3.current.style.transform = "translateY(0)";
-
-    menuElRef4.current.style.opacity = "1";
-    menuElRef4.current.style.transform = "translateY(0)";
-
-    menuElRef5.current.style.opacity = "1";
-    menuElRef5.current.style.transform = "translateY(0)";
-  }
-
-  function collapseMenu() {
-    navIconRef.current.style.opacity = "0";
-    navIconRef.current.style.zIndex = "-10";
-
-    menuElRef1.current.style.opacity = "0";
-    menuElRef1.current.style.transform = "translateY(200%)";
-
-    menuElRef2.current.style.opacity = "0";
-    menuElRef2.current.style.transform = "translateY(200%)";
-
-    menuElRef3.current.style.opacity = "0";
-    menuElRef3.current.style.transform = "translateY(200%)";
-
-    menuElRef4.current.style.opacity = "0";
-    menuElRef4.current.style.transform = "translateY(200%)";
-    
-    menuElRef5.current.style.opacity = "0";
-    menuElRef5.current.style.transform = "translateY(200%)";
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function toggleMobileMenu() {
+    setIsMenuOpen(!isMenuOpen);
   }
 
   return (
@@ -88,32 +46,33 @@ const Navbar = ({ bgColor, textColor }) => {
           </nav>
 
           <div className="relative flex items-center space-x-6 md:hidden">
-            <div onClick={expandMenu} className="absolute right-0 cursor-pointer">
+            <div onClick={toggleMobileMenu} className="absolute right-0 cursor-pointer">
               <i className="text-3xl ri-menu-4-line"></i>
             </div>
           </div>
 
-          <div id="menu-list" className={navStyles.menuList} ref={navIconRef}>
-            <div className="absolute right-10 sm:right-16 top-48 sm:top-44" onClick={collapseMenu}>
+          <div
+            id="menu-list"
+            className={`absolute md:hidden top-0 left-0 w-full h-full bg-black bg-opacity-70 backdrop-blur-xl grid place-content-center text-center ${
+              isMenuOpen ? "opacity-100 z-20" : "-z-20 opacity-0"
+            }`}
+          >
+            <div className="absolute right-10 sm:right-16 top-48 sm:top-44" onClick={toggleMobileMenu}>
               <i className="text-3xl ri-close-line"></i>
             </div>
 
             <ul className="grid h-full my-auto space-y-6 text-2xl text-green-100 place-content-center first:mt-4 last:mb-4">
-              <li className={navStyles.menuElem} ref={menuElRef1}>
+              <li className={navStyles.menuElem}>
                 <Link href="/">Home</Link>
               </li>
-              <li className={navStyles.menuElem} ref={menuElRef2}>
-                Find Match
-              </li>
-              <li className={navStyles.menuElem} ref={menuElRef3}>
+              <li className={navStyles.menuElem}>Find Match</li>
+              <li className={navStyles.menuElem}>
                 <Link href="/auth/signup">Create Account</Link>
               </li>
-              <li className={navStyles.menuElem} ref={menuElRef4}>
+              <li className={navStyles.menuElem}>
                 <Link href="/auth/login">Login</Link>
               </li>
-              <li className={navStyles.menuElem} ref={menuElRef5}>
-                HQ Devs
-              </li>
+              <li className={navStyles.menuElem}>HQ Devs</li>
             </ul>
           </div>
         </div>
